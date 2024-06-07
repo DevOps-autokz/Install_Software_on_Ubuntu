6dd032a6d10f284bfe6569f591ca39f0b831bf87
Auto.kz_DevOps <aituar-s@auto.kz>
2024-06-07 18:34:39 +0500

    Update telegram-desktop.sh

--- dist/telegram-desktop.sh
+++ dist/telegram-desktop.sh
@@ -1,7 +1,8 @@
 if ! [ -x "$(command -v telegram-desktop)" ];
     then
         echo "Telegram installation started..."
-	sudo apt install telegram-desktop -y
+	sudo add-apt-repository ppa:atareao/telegram	
+ 	sudo apt install telegram -y
     else
 	printf '%s\n' "Telegram is already installed"
   exit 0
