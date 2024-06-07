1680e5a39c5184758f772eb5c60ebe5a3569d4c9
Auto.kz_DevOps <aituar-s@auto.kz>
2024-06-07 18:25:08 +0500

    Update install_golang.sh

--- install_golang.sh
+++ install_golang.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Check the last version at:  https://go.dev/dl/
+GOVERSION="1.22.4"
+
 # Download the Go Installation Files (binary archive):
 [ -d /usr/local/go ] && echo "$(go version) is already installed at: $(command -v go)" &&  exit 1
-wget https://go.dev/dl/go1.18.10.linux-amd64.tar.gz
-# curl -OL https:/go.dev/dl/go1.18.10.linux-amd64.tar.gz
+wget https://go.dev/dl/go${GOVERSION}.linux-amd64.tar.gz
+# curl -OL https:/go.dev/dl/go${GOVERSION}.linux-amd64.tar.gz
 
 # Extract the archive and Install Go:
-sudo tar -xzf go1.18.10.linux-amd64.tar.gz -C /usr/local/
+sudo tar -xzf go${GOVERSION}.linux-amd64.tar.gz -C /usr/local/
 
 # Set trap to delete installation archive file on error or exit:
 trap "/usr/bin/rm go1*" ERR EXIT
